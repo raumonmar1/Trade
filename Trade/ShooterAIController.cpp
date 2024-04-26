285643009ccb14f680c96e4255aa625f16176056
raumonmar1 <raumonmar1@alum.us.es>
2024-04-26 18:52:39 +0200

    fix(enemy): fixed enemy follow us

--- Trade/ShooterAIController.cpp
+++ Trade/ShooterAIController.cpp
@@ -20,7 +20,7 @@ void AShooterAIController::Tick(float DeltaSeconds)
     if (LineOfSightTo(PlayerPawn))
     {
         SetFocus(PlayerPawn);
-        MoveToActor(PlayerPawn, 200);
+        MoveToActor(PlayerPawn, AcceptanceRadius);
     }
     else
     {
--- Trade/ShooterAIController.h
+++ Trade/ShooterAIController.h
@@ -20,4 +20,8 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+
+private:
+	UPROPERTY(EditAnywhere)
+	float AcceptanceRadius = 200;
 };
