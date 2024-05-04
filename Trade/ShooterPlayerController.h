4af6cc3b54aaae4207914f888f7a71010bfc81e0
raumonmar1 <raumonmar1@alum.us.es>
2024-05-04 18:35:36 +0200

    feat(improve): Added HUD

--- Trade/ShooterPlayerController.h
+++ Trade/ShooterPlayerController.h
@@ -17,14 +17,24 @@ class TRADE_API AShooterPlayerController : public APlayerController
 public:
 	virtual void GameHasEnded(class AActor* EndGameFocus = nullptr, bool bIsWinner = false) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 private:
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> WinScreenClass;
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> LoseScreenClass;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> HUDClass;
 
 	UPROPERTY(EditAnywhere)
 	float RestartDelay = 5;
 
 	FTimerHandle RestartTimer;
+
+	UPROPERTY()
+	UUserWidget *HUD;
+
 };
