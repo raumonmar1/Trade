0b1f96c4c5ecde1915ba88c067f951f2c5106e0d
raumonmar1 <raumonmar1@alum.us.es>
2024-05-06 12:19:11 +0200

    feat(improve): Introduced health bar on UI

--- Trade/ShooterCharacter.h
+++ Trade/ShooterCharacter.h
@@ -25,6 +25,8 @@ public:
 	UFUNCTION(BlueprintPure)
 	bool IsDead() const;
 
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercent() const;
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
