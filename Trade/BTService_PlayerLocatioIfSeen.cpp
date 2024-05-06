d60c42a53fb08d2481f3a2443b130f4870f7a21b
raumonmar1 <raumonmar1@alum.us.es>
2024-05-06 12:32:09 +0200

    feat(improve): Added rotation on IA controllers

--- Trade/BTService_PlayerLocatioIfSeen.cpp
+++ Trade/BTService_PlayerLocatioIfSeen.cpp
@@ -29,7 +29,7 @@ void UBTService_PlayerLocatioIfSeen::TickNode(UBehaviorTreeComponent& OwnerComp,
     
     if(OwnerComp.GetAIOwner()->LineOfSightTo(PlayerPawn))
     {
-    OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerPawn->GetActorLocation());
+    OwnerComp.GetBlackboardComponent()->SetValueAsObject(GetSelectedBlackboardKey(), PlayerPawn);
     }   
     else
     {
